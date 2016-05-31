1e55423f534be126a6ef7aa2cd83c6b46abb4605
wuzhen <wuzhen@kokozu.net>
2016-05-31 15:11:00 +0800

    commit

--- about.md
+++ about.md
@@ -3,7 +3,7 @@ layout: nav-page
 title: 关于我
 subtitle: <span class="octicon octicon-person"></span>&nbsp;也无风雨也无晴
 menu: about
-css: ['page-about.css', 'sidebar-popular-repo.css', '../../bower_components/flag-icon-css/css/flag-icon.min.css', 'poem.css', 'header-style.css']
+css: ['page-about.css', 'sidebar-popular-repo.css', '../../bower_components/flag-icon-css/css/flag-icon.min.css', 'header-style.css']
 js: ['poem.js']
 ---
 {% include page-about.html %}
--- assets/js/poem.js
+++ assets/js/poem.js
@@ -1,5 +1,25 @@
 // 读取poem.json文件，并且根据日期取一个填充到页面上
 var readPoems = function() {
+    // var s = "";
+    // s += " device width: " + window.innerWidth;
+    // s += " client width: " + document.documentElement.clientWidth;
+    // s += " 网页可见区域宽：" + document.body.clientWidth;
+    // s += " 网页可见区域高：" + document.body.clientHeight;
+    // s += " 网页可见区域宽：" + document.body.offsetWidth + "   (包括边线和滚动条的宽)";
+    // s += " 网页可见区域高：" + document.body.offsetHeight + "   (包括边线的宽)";
+    // s += " 网页正文全文宽：" + document.body.scrollWidth;
+    // s += " 网页正文全文高：" + document.body.scrollHeight;
+    // s += " 网页被卷去的高：" + document.body.scrollTop;
+    // s += " 网页被卷去的左：" + document.body.scrollLeft;
+    // s += " 网页正文部分上：" + window.screenTop;
+    // s += " 网页正文部分左：" + window.screenLeft;
+    // s += " 屏幕分辨率的高：" + window.screen.height;
+    // s += " 屏幕分辨率的宽：" + window.screen.width;
+    // s += " 屏幕可用工作区高度：" + window.screen.availHeight;
+    // s += " 屏幕可用工作区宽度：" + window.screen.availWidth;
+    // s += " 你的屏幕设置是   " + window.screen.colorDepth + "   位彩色";
+    // s += " 你的屏幕设置   " + window.screen.deviceXDPI + "   像素/英寸";
+    // alert(s);
 
     $.getJSON("../../assets/files/poem.json", function(data) {
 
